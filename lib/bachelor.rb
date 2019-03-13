0577bf5f635bb287701e9dd779eff9e9b4a491ff
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-13 23:44:49 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,6 +1,7 @@
 def get_first_name_of_season_winner(data, season)
   # code here
   data.each do |keys, values|
+    return keys
     values.each do |values, info|
       if info != nil
         return info
