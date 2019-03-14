5b23f1bdb90daf7835ccfc49553cb4065b1d32d5
Daniel Hallada <hallada.daniel@gmail.com>
2019-03-14 00:27:48 +0000

    Automatically backed up by Learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -57,7 +57,7 @@ def get_average_age_for_season(data, season)
   data.each do |keys, values|
     if keys == season
       values.each do |values|
-        ages.push(values["age"].to_i)
+        ages.push(values["age"].to_f)
       end
       end
     end
